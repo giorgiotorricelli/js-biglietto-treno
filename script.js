be4d47a0cde026e52a2e35f8b7c2b9c5190c8f95
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-23 18:09:25 +0100

    aggiunto l'algoritmo per il prezzo del biglietto e rifinito l'output

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const kilometersAsString = prompt("inserisci la distanza che vuoi percorrere (in kilometri): ");
 const kilometersAsInt = parseInt(kilometersAsString);
-console.log(kilometersAsInt);
 
 const ageAsString = prompt("inserisci la tua età: ");
 const ageAsInt = parseInt(ageAsString);
-console.log(ageAsInt);
+
+
+const euroPerKilometer = 0.21;
+
+let prezzoSenzaSconto = kilometersAsInt * euroPerKilometer;
+let prezzoScontato;
+
+if (ageAsInt < 18) {
+    prezzoScontato = (prezzoSenzaSconto * 0.8).toFixed(2);
+} else if (ageAsInt > 65) {
+    prezzoScontato = (prezzoSenzaSconto * 0.6).toFixed(2);
+} else {
+    prezzoScontato = prezzoSenzaSconto.toFixed(2);
+}
+
+console.log(`Il prezzo del biglietto è di ${prezzoScontato}\u20AC`);
