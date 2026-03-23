8f1f09611544eef02a9cbf2d7279bb072187b89c
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-23 17:37:34 +0100

    dichiarazione e assegnamento delle variabili

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("ciao")
+const kilometersAsString = prompt("inserisci la distanza che vuoi percorrere (in kilometri): ");
+const kilometersAsInt = parseInt(kilometersAsString);
+console.log(kilometersAsInt);
+
+const ageAsString = prompt("inserisci la tua età: ");
+const ageAsInt = parseInt(ageAsString);
+console.log(ageAsInt);
